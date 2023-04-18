45efa63d7db549b075445f69d47a3b4b43c6f99a
Levi Eggert <levi.eggert@cru.org>
2023-04-18 12:30:25 -0400

    Add dependencies to package.swift Add Facebook auth classes

---
+++ Sources/SocialAuthentication/FacebookAuthentication/FacebookAuthentication+Combine.swift
@@ -0,0 +1,13 @@
+//
+//  FacebookAuthentication+Combine.swift
+//  SocialAuthentication
+//
+//  Created by Levi Eggert on 4/18/23.
+//  Copyright © 2023 Cru Global, Inc. All rights reserved.
+//
+
+import Foundation
+
+public extension FacebookAuthentication {
+    
+}
---
+++ Sources/SocialAuthentication/FacebookAuthentication/FacebookAuthenticationConfiguration.swift
@@ -0,0 +1,16 @@
+//
+//  FacebookAuthenticationConfiguration.swift
+//  SocialAuthentication
+//
+//  Created by Levi Eggert on 4/18/23.
+//  Copyright © 2023 Cru Global, Inc. All rights reserved.
+//
+
+import Foundation
+
+public class FacebookAuthenticationConfiguration {
+    
+    public init() {
+        
+    }
+}
