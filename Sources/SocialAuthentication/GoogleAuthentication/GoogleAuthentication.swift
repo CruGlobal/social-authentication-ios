bb2d94a396f86e642f977ef766f5de4a3affd0ef
Levi Eggert <levi.eggert@cru.org>
2023-05-08 10:55:23 -0400

    Add file GoogleAuthentication.swift

---
+++ Sources/SocialAuthentication/GoogleAuthentication/GoogleAuthentication.swift
@@ -0,0 +1,23 @@
+//
+//  GoogleAuthentication.swift
+//  SocialAuthentication
+//
+//  Created by Levi Eggert on 5/8/23.
+//  Copyright © 2023 Cru Global, Inc. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+
+public class GoogleAuthentication {
+    
+    private let sharedGoogleSignIn: GIDSignIn = GIDSignIn.sharedInstance
+    
+    public init() {
+        
+    }
+    
+    public func authenticate() {
+        
+    }
+}
