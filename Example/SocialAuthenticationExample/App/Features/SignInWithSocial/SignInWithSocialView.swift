f23564369fed6498ad924107ddd8acd1470be629
Levi Eggert <levi.eggert@cru.org>
2023-05-09 10:37:53 -0400

    Add google sign in button and status to view

--- Example/SocialAuthenticationExample/App/Features/SignInWithSocial/SignInWithSocialView.swift
+++ Example/SocialAuthenticationExample/App/Features/SignInWithSocial/SignInWithSocialView.swift
@@ -10,11 +10,6 @@ import SwiftUI
 
 struct SignInWithSocialView: View {
     
-    private let buttonFont: Font = Font.system(size: 16, weight: .semibold)
-    private let buttonHeight: CGFloat = 50
-    private let buttonInsets: EdgeInsets = EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30)
-    private let buttonCornerRadius: CGFloat = 8
-    
     @ObservedObject private var viewModel: SignInWithSocialViewModel
     
     init(viewModel: SignInWithSocialViewModel) {
@@ -25,9 +20,7 @@ struct SignInWithSocialView: View {
     var body: some View {
         
         GeometryReader { geometry in
-            
-            let buttonWidth: CGFloat = geometry.size.width - buttonInsets.leading - buttonInsets.trailing
-            
+                
             VStack(alignment: .center, spacing: 0) {
                 
                 Text("Sign-in With Social")
@@ -36,117 +29,25 @@ struct SignInWithSocialView: View {
                     .multilineTextAlignment(.center)
                     .padding(EdgeInsets(top: 50, leading: 30, bottom: 0, trailing: 30))
                 
-                HStack(alignment: .center, spacing: 10) {
-                    
-                    Text("Facebook has persisted access token: ")
-                        .font(Font.system(size: 17))
-                        .foregroundColor(Color.black)
-                        .multilineTextAlignment(.leading)
-                    
-                    Rectangle()
-                        .fill(viewModel.facebookHasPersistedAccessToken ? Color.green : Color.red)
-                        .frame(width: 15, height: 15)
-                }
-                .padding(EdgeInsets(top: 40, leading: 30, bottom: 0, trailing: 30))
+                SocialSignInStatusView(
+                    title: "Facebook has persisted access token: ",
+                    isSuccessStatus: viewModel.facebookHasPersistedAccessToken
+                )
                 
-                HStack(alignment: .center, spacing: 10) {
-                    
-                    Text("Apple is authenticated: ")
-                        .font(Font.system(size: 17))
-                        .foregroundColor(Color.black)
-                        .multilineTextAlignment(.leading)
-                    
-                    Rectangle()
-                        .fill(viewModel.appleIsAuthenticated ? Color.green : Color.red)
-                        .frame(width: 15, height: 15)
-                }
-                .padding(EdgeInsets(top: 40, leading: 30, bottom: 0, trailing: 30))
+                SocialSignInStatusView(
+                    title: "Apple is authenticated: ",
+                    isSuccessStatus: viewModel.appleIsAuthenticated
+                )
+                
+                SocialSignInStatusView(
+                    title: "Google is authenticated: ",
+                    isSuccessStatus: viewModel.googleIsAuthenticated
+                )
                 
                 Spacer()
                 
-                Button(action: {
-                    
-                    viewModel.signInWithFacebookTapped()
-                    
-                }) {
-                    
-                    ZStack(alignment: .center) {
-                        
-                        Rectangle()
-                            .fill(.clear)
-                            .frame(width: buttonWidth, height: buttonHeight)
-                            .cornerRadius(buttonCornerRadius)
-                        
-                        Text("Login with Facebook")
-                            .font(buttonFont)
-                            .foregroundColor(Color.white)
-                            .padding()
-                    }
-                }
-                .frame(width: buttonWidth, height: buttonHeight, alignment: .center)
-                .background(Color.gray)
-                .cornerRadius(buttonCornerRadius)
-                .padding(buttonInsets)
-                
-                Rectangle()
-                    .fill(Color.clear)
-                    .frame(width: 1, height: 15)
-                
-                Button(action: {
-                    
-                    viewModel.signInWithGoogleTapped()
-                    
-                }) {
-                    
-                    ZStack(alignment: .center) {
-                        
-                        Rectangle()
-                            .fill(.clear)
-                            .frame(width: buttonWidth, height: buttonHeight)
-                            .cornerRadius(buttonCornerRadius)
-                        
-                        Text("Sign in with Google")
-                            .font(buttonFont)
-                            .foregroundColor(Color.white)
-                            .padding()
-                    }
-                }
-                .frame(width: buttonWidth, height: buttonHeight, alignment: .center)
-                .background(Color.gray)
-                .cornerRadius(buttonCornerRadius)
-                .padding(buttonInsets)
-                
-                Rectangle()
-                    .fill(Color.clear)
-                    .frame(width: 1, height: 15)
-                
-                Button(action: {
-                    
-                    viewModel.signInWithAppleTapped()
-                    
-                }) {
-                    
-                    ZStack(alignment: .center) {
-                        
-                        Rectangle()
-                            .fill(.clear)
-                            .frame(width: buttonWidth, height: buttonHeight)
-                            .cornerRadius(buttonCornerRadius)
-                        
-                        Text("Continue with Apple")
-                            .font(buttonFont)
-                            .foregroundColor(Color.white)
-                            .padding()
-                    }
-                }
-                .frame(width: buttonWidth, height: buttonHeight, alignment: .center)
-                .background(Color.gray)
-                .cornerRadius(buttonCornerRadius)
-                .padding(buttonInsets)
-                
-                Rectangle()
-                    .fill(Color.clear)
-                    .frame(width: 1, height: 40)
+                SocialSignInButtonsView(viewModel: viewModel, geometry: geometry)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 40, trailing: 0))
             }
         }
     }
