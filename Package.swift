2ee791d67d85c019dbf3aab1234c01518e06debe
Levi Eggert <levi.eggert@cru.org>
2025-10-16 09:18:38 -0400

    Update facebook dependency and min swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["SocialAuthentication"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", .upToNextMinor(from: "18.0.0")),
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", .upToNextMinor(from: "18.0.1")),
         .package(url: "https://github.com/google/GoogleSignIn-iOS.git", .upToNextMinor(from: "9.0.0"))
     ],
     targets: [
